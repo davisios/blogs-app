a0dbbf2194a35b5508ac0070c772ead9d6142cb6
David Leon ALvarez <david.leon@nursa.com>
2024-03-18 10:31:38 -0600

    test: lint file to pass super lint

--- client/src/services/blogService.js
+++ client/src/services/blogService.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
 const getBlogs = async () => {
-  return axios.get(    `${process.env.REACT_APP_API_URL}/blogs`);
+  return axios.get(`${process.env.REACT_APP_API_URL}/blogs`);
 };
- 
+
 const deleteBlog = async (id) => {
-  const test = 'asd   ';
-
-
-
-  return  axios.delete(`${process.env.REACT_APP_API_URL}/blogs/${id}`);
+  return axios.delete(`${process.env.REACT_APP_API_URL}/blogs/${id}`);
 };
 
 export { getBlogs, deleteBlog };
