0a2c22518d2d8ae2c33d04b4072b57b9869badda
David Leon ALvarez <david.leon@nursa.com>
2024-03-18 10:29:01 -0600

    test: fail lint

--- client/src/services/blogService.js
+++ client/src/services/blogService.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 const getBlogs = async () => {
-  return axios.get(`${process.env.REACT_APP_API_URL}/blogs`);
+  return axios.get(    `${process.env.REACT_APP_API_URL}/blogs`);
 };
-
+ 
 const deleteBlog = async (id) => {
-  return axios.delete(`${process.env.REACT_APP_API_URL}/blogs/${id}`);
+  const test = 'asd   ';
+
+
+
+  return  axios.delete(`${process.env.REACT_APP_API_URL}/blogs/${id}`);
 };
 
 export { getBlogs, deleteBlog };
